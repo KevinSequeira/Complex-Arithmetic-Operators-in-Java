db815d4a38e81e254873fc7342ce98e1ace263e7
Kevin Sequeira <kevin.sequeira.professional@gmail.com>
2017-03-11 18:27:05 +0530

    Create complexAritmeticOperations.java
    001 Consider the following program which explains the incremend and decrement arithmetic operators:
    
    public class complexArithmeticOperations {
    
    	public static void main(String[] args) {
    		// TODO Auto-generated method stub
    
    		//Understanding the Increment Operator
    		int a = 40;
    		System.out.println("Original value of A: " + a);
    		
    		int b = a++;
    		System.out.println("Value of A during post-increment operation: " + b);
    		System.out.println("Value of A after post-increment operation: " + a);
    		
    		int c = ++a;
    		System.out.println("Value of A during pre-increment operation: " + c);
    		System.out.println("Value of A after pre-increment operation: " + a);
    		System.out.println();
    		
    		//Understanding the Decrement Operator
    		int x = 40;
    		System.out.println("Original value of X: " + x);
    		
    		int y = x--;
    		System.out.println("Value of X during post-decrement operation: " + y);
    		System.out.println("Value of X after post-decrement operation: " + x);
    		
    		int z = --x;
    		System.out.println("Value of X during pre-decrement operation: " + z);
    		System.out.println("Value of X after pre-decrement operation: " + x);
    	
    	}
    
    }
    
    002) The above code provides the following output:
    
    Original value of A: 40
    Value of A during post-increment operation: 40
    Value of A after post-increment operation: 41
    Value of A during pre-increment operation: 42
    Value of A after pre-increment operation: 42
    
    Original value of X: 40
    Value of X during post-decrement operation: 40
    Value of X after post-decrement operation: 39
    Value of X during pre-decrement operation: 38
    Value of X after pre-decrement operation: 38

---
+++ complexAritmeticOperations.java
@@ -0,0 +1,36 @@
+# Complex-Arithmetic-Operators-in-Java
+Hey everyone. Here's an example program demonstrating how Complex Arithmetic Operators work in Java.
+
+public class complexArithmeticOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Understanding the Increment Operator
+		int a = 40;
+		System.out.println("Original value of A: " + a);
+		
+		int b = a++;
+		System.out.println("Value of A during post-increment operation: " + b);
+		System.out.println("Value of A after post-increment operation: " + a);
+		
+		int c = ++a;
+		System.out.println("Value of A during pre-increment operation: " + c);
+		System.out.println("Value of A after pre-increment operation: " + a);
+		System.out.println();
+		
+		//Understanding the Decrement Operator
+		int x = 40;
+		System.out.println("Original value of X: " + x);
+		
+		int y = x--;
+		System.out.println("Value of X during post-decrement operation: " + y);
+		System.out.println("Value of X after post-decrement operation: " + x);
+		
+		int z = --x;
+		System.out.println("Value of X during pre-decrement operation: " + z);
+		System.out.println("Value of X after pre-decrement operation: " + x);
+	
+	}
+
+}
